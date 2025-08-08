efbf095178eafa1d85b0424826c103290a1d8425
thanyarad <thanyarad>
2025-08-08 04:03:00 +0000

    bronze_layer

--- transformations/bronze/cpg_order_items.py
+++ transformations/bronze/cpg_order_items.py
@@ -2,7 +2,7 @@ from pyspark.sql.functions import input_file_name, current_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType
 
 # Define schema for cpg_order_items
-cpg_order_items_schema = StructType([
+cpg_consumer_order_items_schema = StructType([
     StructField("order_item_id", StringType()),
     StructField("order_id", StringType()),
     StructField("product_id", StringType()),
@@ -15,8 +15,8 @@ cpg_order_items_schema = StructType([
 # Load JSON from volume
 df_raw = (
     spark.read
-    .schema(cpg_order_items_schema)
-    .json("dbfs:/Volumes/cpg_industry/cpg_data/data/cpg_consumer/cpg_order_items.json")
+    .schema(cpg_consumer_order_items_schema)
+    .json("dbfs:/Volumes/cpg_industry/cpg_data/data/cpg_consumer/cpg_consumer_order_items.json")
 )
 
 # Write to Delta Table
